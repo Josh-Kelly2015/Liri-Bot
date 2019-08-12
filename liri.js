23ecdb0ec285a6bbbf006dea3308e3a85f914bda
Joshua Kelly <jdkelly2015@gmail.com>
2019-08-12 18:40:27 -0400

    i was going it alllll wrong

--- liri.js
+++ liri.js
@@ -1,4 +1,38 @@
 require("dotenv").config();
-var keys = require("./keys");
-var spotify = new spotify(keys.spotify);
+var fs = require("file-system");
+var axios = require("axios");
 
+var keys = require("./keys");
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+// var songName = process.argv.split(3).join(" ");
+var apiQuery = process.argv[2];
+
+if (apiQuery == "spotify-this-song") {
+    //Create contructor function
+    var SpotifyQuery = function () {
+        //apparently divider is a good idea
+        var divider = "/n .......................... /n";
+        //Make up my own function name to search spotify
+        this.findSong = song => {
+            song = process.argv.split(3).join(" ");
+            spotify.search({ type: 'track', query: song }, (err, data) => {
+                if (err) {
+                    return console.log(divider + 'Error occurred: ' + err + divider);
+                }
+
+                var jsonData = data;
+                var printData = [
+                    "Artist(s): " + jsonData,
+                    "Song Name: ",
+                    "Preview Link: ",
+                    "Album: ",
+                ].join("/n");
+
+                fs.appendFile("log.txt", printData + divider, err => { if (err) throw err; console.log(printData); })
+            });
+        };
+    };
+};
+
+module.exports = SpotifyQuery;
