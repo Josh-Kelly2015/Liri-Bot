d964f5fc4dbea0e7c6fa5f7eb05cb2efd3e94af4
Joshua Kelly <jdkelly2015@gmail.com>
2019-08-13 16:17:38 -0400

    i was so wrong about so much. i overcomplicated what was simple. spotify API is done after 2 days of bullshit. if i can query at least 1 more API tonight ill be happy.

--- liri.js
+++ liri.js
@@ -1,33 +1,48 @@
 require("dotenv").config();
-var fs = require("file-system");
-var axios = require("axios");
+// var fs = require("file-system");
+// var axios = require("axios");
 
+// These variables are for SpotifyQuery
 var keys = require("./keys");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
-
-//Create contructor function
-var SpotifyQuery = function () {
-    //apparently divider is a good idea
-    var divider = "/n .......................... /n";
-    //Make up my own function name to search spotify
-    this.findSong = song => {
-
-        spotify.search({ type: 'track', query: song, limit: 5 }).then(response => console.log(response)).catch(err => console.log(err));
-
-        var jsonData = response;
-        var songData = [
-            "Artist(s): " + jsonData,
-            "Song Name: ",
-            "Preview Link: ",
-            "Album: ",
-        ].join("/n");
-        console.log(songData);
-
-        fs.appendFile("log.txt", songData + divider, err => { if (err) throw err; console.log(songData); })
-
-    };
+// search var will be used everywhere
+var search = process.argv[2];
+//Divider to help organize the response
+var divider = ".....................";
+//Search term
+var song = process.argv.slice(3).join(" ");
+//if statements to make the query
+if (!search) {
+    search = "spotify-this-song";
 };
+if (!song) {
+    song = "The Sign";
+};
+if (search == "spotify-this-song") {
+    console.log("\n Searching for Song Info\n>\n>\n>");
+};
+//search function given via Spotify docs
+spotify.search({ type: 'track', query: song, limit: 3 }).then(response => console.log(response)).catch(err => console.log(err));
+//create variables containing JSON path from the response. add to songData array
+var artists //list of artist(s) song was sung by
+var songName // song name
+var link //preview link
+var album // list of album(s) song appeared on  
+//variable to format response from the search request
+var songData = [
+    "Artist(s): " ,
+    "Song Name: " ,
+    "Preview Link: ",
+    "Album: ",
+].join("\n");
+
+ 
+console.log(divider + " Info on: " + "'" + song + "' " + divider + "\n\n" + songData + "\n\n" + divider + divider + divider +"\n\n\n");
 
 
-module.exports = SpotifyQuery;
+
+
+
+
+
